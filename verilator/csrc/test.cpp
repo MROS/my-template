607cf7501b372085112de971f52e6c6649b0b7b2
MROS <yc1043@gmail.com>
2024-07-25 17:02:54 +0800

    verilator C++ test 正確縮排

--- verilator/csrc/test.cpp
+++ verilator/csrc/test.cpp
@@ -5,22 +5,23 @@
 #include <stdlib.h>
 #include <assert.h>
 
-int main() {
-		Vtop *top = new Vtop();
+int main()
+{
+	Vtop *top = new Vtop();
 
-		Verilated::traceEverOn(true);
-		VerilatedVcdC *m_trace = new VerilatedVcdC;
-		top->trace(m_trace, 5);
-		m_trace->open("waveform.vcd");
+	Verilated::traceEverOn(true);
+	VerilatedVcdC *m_trace = new VerilatedVcdC;
+	top->trace(m_trace, 5);
+	m_trace->open("waveform.vcd");
 
-		top->en = 1;
+	top->en = 1;
 
-		for (int i = 0; i < 4; i++) {
-			top->x = i;
-			top->eval();
-			assert(top->y == (1 << i));
-			m_trace->dump(i);
-		}
-		m_trace->close();
+	for (int i = 0; i < 4; i++)
+	{
+		top->x = i;
+		top->eval();
+		assert(top->y == (1 << i));
+		m_trace->dump(i);
+	}
+	m_trace->close();
 }
-
